87db77e4ffb157a49b29f3ad60f05165360ebf4c
N1cc3 <niclaslindgren91@hotmail.com>
2022-11-27 02:59:39 +0200

    Add react

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import replace from '@rollup/plugin-replace';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       plugins: [
